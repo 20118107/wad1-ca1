1d0de5283646dfdd97903f693ea989f98f11b487
20118107 <20118107@setu.ie>
2026-03-09 00:04:10 +0000

    update

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,40 +1,19 @@
-// import books store which contains functions for getting book data
 import booksStore from '../models/books-store.js';
 
-// controller that for handles book and genre routes
-const booksController = {
+const dashboardController = {
+  createView(request, response) {
 
-  // shows all books belonging to a specific genre
-  viewGenre(req, res) {
+    const allBooks = booksStore.getAllBooks();
 
-    // gets the genre name from the url parameter
-    const genreName = req.params.genre;
+    const genres = [...new Set(allBooks.map(book => book.genre))];
 
-    // get all books from store and filter them by the genre
-    const books = booksStore.getAllBooks().filter(b => b.genre === genreName);
+    const viewData = {
+      title: 'Library Dashboard',
+      genres: genres
+    };
 
-    // render the genre view and pass the books that apply
-    res.render('genre', {
-      title: `${genreName} Books`,
-      books
-    });
-  },
-
-
-  // shows the detailed page for a single book
-  viewBook(req, res) {
-
-    // retrieves the book id from the url parameter
-    const bookId = req.params.id;
-
-    // uses the books store to find the book with the matching id
-    const book = booksStore.getBookById(bookId);
-
-
-    // rendesr the book view and passes the book data
-    res.render('book', { title: book.title, book });
+    response.render('dashboard', viewData);
   }
 };
 
-// export controller so it can be used in routes
-export default booksController;
+export default dashboardController;
--- routes.js
+++ routes.js
@@ -2,22 +2,21 @@
 
 import express from 'express';
 import logger from "./utils/logger.js";
+
+import start from './controllers/start.js';
+import dashboard from "./controllers/dashboard.js";
 import booksController from './controllers/books.js';
 import genreController from './controllers/genre.js';
+import about from "./controllers/about.js";
 
 const router = express.Router();
 
-import start from './controllers/start.js';
+// routes
 router.get('/', start.createView);
-
-import dashboard from "./controllers/dashboard.js";
 router.get("/dashboard", dashboard.createView);
-
 router.get('/genre/:genre', genreController.createView);
-
 router.get("/book/:id", booksController.viewBook);
-
-import about from "./controllers/about.js";
 router.get("/about", about.createView);
 
 export default router;
+
